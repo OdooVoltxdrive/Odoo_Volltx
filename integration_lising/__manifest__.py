8f14b75e0b5123dfa67e343a1ff4abfb3acc4eaf
oc-mmillan <mmillan@odooconcept.com>
2026-06-04 13:45:31 -0400

    feat_update module integracion

---
+++ integration_lising/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    # Module Information
+    "name": "Fleet Vehicle voltx drive",
+    "category": "Fleet Rent",
+    "version": "19.0",
+    "license": "LGPL-3",
+    "summary": """Sistema de gestion de leasing""",
+    "sequence": 1,
+    "author": "Silicon Valley latamn , Colaborador:Ing.Marilyn Millan",
+    "website": "www.siliconvalleyve.com",
+    "depends": ["base","fleet", "contacts"],
+    "data": [
+        #"security/ir.model.access.csv",
+        "views/res_partner_view.xml",
+    ],
+    "images": ["static/description/icon.jpg"],
+
+
+    'demo': [],
+    'license': 'LGPL-3',
+    'application': True,
+}
+
+# -*- coding: utf.8 -*-
+
+
