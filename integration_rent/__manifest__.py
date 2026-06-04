f599eb6ddc5079877db29c21cd72954229818972
oc-mmillan <mmillan@odooconcept.com>
2026-06-04 13:40:07 -0400

    feat:update manifest 02

--- integration_rent/__manifest__.py
+++ integration_rent/__manifest__.py
@@ -1,6 +1,6 @@
 {
     # Module Information
-    "name": "Fleet Rental Vehicle voltx drive",
+    "name": "Fleet Vehicle voltx drive",
     "category": "Fleet Rent",
     "version": "19.0",
     "license": "LGPL-3",
@@ -14,17 +14,13 @@
         "views/res_partner_view.xml",
     ],
     "images": ["static/description/icon.jpg"],
-    "assets": {
-        #"web.assets_backend": [
-            #"fleet_rent/static/src/css/fleet_rent.scss",
-            #"fleet_rent/static/src/css/rent_order.css",
-        #],
-    },
 
 
     'demo': [],
     'license': 'LGPL-3',
     'application': True,
 }
+
+# -*- coding: utf.8 -*-
 
 
