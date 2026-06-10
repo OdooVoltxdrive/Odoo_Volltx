e55c5b77ef97754d6a69d543d7dfcf222319e982
oc-mmillan <mmillan@odooconcept.com>
2026-06-10 16:01:47 -0400

    plan de cuotas 02

--- integration_lising/models/fleet_vehicle_contract_line.py
+++ integration_lising/models/fleet_vehicle_contract_line.py
@@ -1,6 +1,6 @@
 # See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 class FleetVehicleContractLine(models.Model):
     _name = "fleet.vehicle.contract.line"
